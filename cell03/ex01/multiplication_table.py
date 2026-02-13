29b3d8bbf677fab3cf1e73245b3802a914cf334d
SIWCAMPL <67080309@kmitl.ac.th>
2026-02-13 09:28:42 +0000

    Force update solution

---
+++ cell03/ex01/multiplication_table.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+num = int(input("Enter a number : "))
+i = 0
+while(i <= 9):
+    print(f"{i} * {num} = {i*num}")
+    i += 1
---
+++ cell03/ex02/i_got_that.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+camtop = input("What you gotta say? : ")
+
+while True:
+    if(camtop == "STOP"):
+         break
+    camtop = input("I got that! Anything else? : ")
+        
